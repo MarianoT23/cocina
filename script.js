4b6ffea8cb36c949cd5c89f902e6363b86395db9
Mariano Timistit <mtimistit@ius.austral.edu.ar>
2020-05-23 18:02:34 -0300

    coreciones

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
