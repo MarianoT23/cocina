aeb1024e33295d0db773c9aa8aaeadd9504924e9
Mariano Timistit <mtimistit@ius.austral.edu.ar>
2020-07-06 12:09:37 -0300

    correciones

--- JS/script-index.js
+++ JS/script-index.js
@@ -1,10 +1,14 @@
 ////////////////////////////// HAMBURGER MENU
 
 var miniNav = document.getElementById("mini-nav");
+var hambBtn = document.getElementById("hamb-menu")
 function hambMenu() {
     if (miniNav.style.display === "block") {
         miniNav.style.display = "none";
+        hambBtn.classList.remove("open");
     } else {
         miniNav.style.display = "block";
+        hambBtn.classList.add("open");
     }
 };
+
